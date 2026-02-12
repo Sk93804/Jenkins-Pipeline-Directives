44ffb75cb2555f8502fc67edb417e9dec3773d47
Sk93804 <sudheesh.zx@gmail.com>
2026-02-12 14:54:33 +0530

    Update

--- JenkinsFile-parameters/parms.groovy
+++ JenkinsFile-parameters/parms.groovy
@@ -1,32 +1,34 @@
-pipeline{
+pipeline {
     agent any 
-    parameters{
+    parameters {
         string(name: 'DB_NAME', defaultValue: 'production', description: 'Choose the database name')
-        booleanParam(name:'CONFIRM_MIGRATION', defaultValue: true , description: 'Do you want to migrate?')
+        booleanParam(name: 'CONFIRM_MIGRATION', defaultValue: true, description: 'Do you want to migrate?')
     }
-    stages{
-        stage('Database Migration'){
-            when{
-                allOf{ expression {params.DB_NAME == 'production'}
-                       expression  {params.CONFIRM_MIGRATION == true }
-            }
-            steps{
-                echo "executing database migration in ${params.DB_NAME}"
-            }
-        }
-    }
-    stage('Skipping Migration Notification'){
-        when{
-            not{
-                allOf{
-                     expression {params.DB_NAME == 'production'}
-                     expression  {params.CONFIRM_MIGRATION == true }
+    stages {
+        stage('Database Migration') {
+            when {
+                allOf { 
+                    expression { params.DB_NAME == 'production' }
+                    expression { params.CONFIRM_MIGRATION == true }
                 }
             }
-        }
-        steps{
-            echo " Skipping migration: Criteria not met for ${params.DB_NAME}"
-        }
-    }
-}
-}
+            steps {
+                echo "executing database migration in ${params.DB_NAME}"
+            }
+        } // End of Database Migration stage
+
+        stage('Skipping Migration Notification') {
+            when {
+                not {
+                    allOf {
+                        expression { params.DB_NAME == 'production' }
+                        expression { params.CONFIRM_MIGRATION == true }
+                    }
+                }
+            }
+            steps {
+                echo "Skipping migration: Criteria not met for ${params.DB_NAME}"
+            }
+        } // End of Skipping Migration Notification stage
+    } // End of stages
+} // End of pipeline
