95d3a7b14a62f6035e3284e55812bb5c73d76565
Sk93804 <sudheesh.zx@gmail.com>
2026-02-12 14:49:50 +0530

    Update

---
+++ JenkinsFile-parameters/parms.groovy
@@ -0,0 +1,31 @@
+pipeline{
+    agent any 
+    parameters{
+        string(name: 'DB_NAME', defaultValue: 'production', description: 'Choose the database name'),
+        booleanParam(name:'CONFIRM_MIGRATION', defaultValue: true , description: 'Do you want to migrate?')
+    }
+    stages{
+        stage('Database Migration'){
+            when{
+                allOf{ expression {params.DB_NAME == 'production'}
+                       expression  {params.CONFIRM_MIGRATION == true }
+            }
+            steps{
+                echo "executing database migration in ${params.DB_NAME}"
+            }
+        }
+    }
+    stage('Skipping Migration Notification'){
+        when{
+            not{
+                allOf{
+                     expression {params.DB_NAME == 'production'}
+                     expression  {params.CONFIRM_MIGRATION == true }
+                }
+            }
+        }
+        steps{
+            echo " Skipping migration: Criteria not met for ${params.DB_NAME}"
+        }
+    }
+}
