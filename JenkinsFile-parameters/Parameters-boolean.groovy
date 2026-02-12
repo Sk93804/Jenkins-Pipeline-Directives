688cdc826254326e2eb5af87f1dc049be78bb91f
Sk93804 <sudheesh.zx@gmail.com>
2026-02-12 19:48:18 +0530

    Update

--- JenkinsFile-parameters/Parameters-boolean.groovy
+++ JenkinsFile-parameters/Parameters-boolean.groovy
@@ -1,9 +1,12 @@
 pipeline {
     agent any 
     parameters {
-        booleanParam(name: 'SKIP_TESTS', 
-        defaultValue: false, 
-        description: 'Do you want to skip the tests?')
+        booleanParam(name: 'SKIP_UNIT_TESTS', 
+                     defaultValue: false, 
+                     description: 'Do you want to skip the tests?')
+        booleanParam(name: 'SKIP_PERFORMENCE_TESTS'
+                     defaultValue: false 
+                     description: 'Do you want to skip the performence tests?') 
     }
     stages {
         stage('checkout') {
@@ -13,18 +16,19 @@ pipeline {
         }
 
         stage('Tests') {
-            when {
-                expression { 
-                    params.SKIP_TESTS.toBoolean() == false
-                }
-            }
             parallel{
                 stage('Unit-test'){
+                    when{
+                        expression{ params.SKIP_UNIT_TESTS.toBoolean() == false }
+                    }
                     steps{
                         echo "executing the unit tets"
                     }
                 }
                 stage('Performence-test'){
+                    when{
+                        expression{ params.SKIP_PERFORMENCE_TESTS.toBoolean() == false}
+                    }
                     steps{
                         echo "Executing the performence tests"
                     }
